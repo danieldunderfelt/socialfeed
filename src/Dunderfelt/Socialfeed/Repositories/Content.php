85fba56cff0f5ba89607080666f543ccb413a730
Daniel Dunderfelt <dunderfeltdaniel@gmail.com>
2014-09-08 19:30:29 +0300

    whoops indeed

--- src/Dunderfelt/Socialfeed/Repositories/Content.php
+++ src/Dunderfelt/Socialfeed/Repositories/Content.php
@@ -1,6 +1,9 @@
 <?php namespace Dunderfelt\Socialfeed\Repositories;
 
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
 class Content extends \Eloquent {
+    use SoftDeletingTrait;
 
     protected $table = 'social_feed_content';
     protected $guarded = ['id'];
---
+++ src/migrations/2014_09_07_175644_create_social_feed_content_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialFeedContentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('social_feed_content', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('content_id')->unique();
+            $table->integer('content_created');
+            $table->text('content_text')->nullable();
+            $table->string('content_creator')->nullable();
+            $table->text('content_creator_name')->nullable();
+            $table->integer('shown')->default(0);
+            $table->text('hashtags')->nullable();
+            $table->integer('approved')->default(0);
+            $table->string('media_url')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('social_feed_content');
+	}
+
+}
